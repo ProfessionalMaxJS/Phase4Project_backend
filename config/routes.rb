99edc31a88ea5897d2125ba604c8f920e2de53c2
Max <max.edison@yahoo.com>
2021-12-20 12:39:44 -0500

    initial tables, seed data

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  resources :selected_items
+  resources :purchased_items
+  resources :items
+  resources :users
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+end
